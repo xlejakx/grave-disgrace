a350b9fa01297a25c9eda3f4174bebbe50877424
castor <donbobrino@gmail.com>
2019-03-26 12:23:26 +0300

    yo

---
+++ form.js
@@ -0,0 +1,33 @@
+
+
+
+$('#bootstrapForm').submit(function (event) {
+    event.preventDefault()
+    var extraData = {}
+    $('#bootstrapForm').ajaxSubmit({
+        data: extraData,
+        dataType: 'jsonp',  // This won't really work. It's just to use a GET instead of a POST to allow cookies from different domain.
+        error: function () {
+            // Submit of form should be successful but JSONP callback will fail because Google Forms
+            // does not support it, so this is handled as a failure.
+            // alert('Form Submitted. Thanks.')
+            // You can also redirect the user to a custom thank-you page:
+            modal.style.display = "flex"
+        }
+    })
+})
+
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName('close')[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        document.getElementById("559352220").value = "";
+        document.getElementById("1275419724").value = "Москва";
+    }
+}
